ad8df9ba2d911c7aeb72af840e7f9508db770e50
nouranfarahat <nouranfarahat2000@gmail.com>
2023-06-01 14:25:22 +0300

    add offline and modify some features

--- app/src/main/java/com/example/foodplanner/mainscreen/SplashScreen.java
+++ app/src/main/java/com/example/foodplanner/mainscreen/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.foodplanner.mainscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -13,6 +14,8 @@ import com.example.foodplanner.signup.view.SignUpActivity;
 
 public class SplashScreen extends AppCompatActivity {
     LottieAnimationView lottieAnimationView;
+    public static final String PREF_NAME="APPINFO";
+
 
     private Handler mHandler = new Handler();
     @Override
@@ -25,6 +28,12 @@ public class SplashScreen extends AppCompatActivity {
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                SharedPreferences pref=getSharedPreferences(PREF_NAME,0);
+
+                //String login=pref.getString("LOGIN","unknown");
+                //if(login.equals("YES"))
+                  //  startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                //else
                 startActivity(new Intent(SplashScreen.this, MainSignUpActivity.class));
                 finish();
             }
